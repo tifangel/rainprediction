a245466933d91c118d5b2bd4e95bb91cea7645b0
anindya <pamsrewari@gmail.com>
2022-04-12 16:08:57 +0700

    Init FE files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "@material-tailwind/react/tailwind.css";
+import './App.css';
+import CardTest from './components/CardTest';
+
+function App() {
+  return (
+    <div className="App">
+      <CardTest />
+    </div>
+  );
+}
+
+export default App;
